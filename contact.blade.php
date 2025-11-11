a151db2cab57961f2a1138f7a598719664d78658
WAHYU INDRAWAN <161592016+VVahyuidr@users.noreply.github.com>
2025-11-12 00:03:08 +0800

    Add files via upload

---
+++ contact.blade.php
@@ -0,0 +1,22 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <h2 class="text-2xl font-semibold text-white mb-6">
+        You can contact me:
+    </h2>
+
+    <div class="flex flex-col gap-4">
+        <a href="https://wa.me/6285338663550" target="_blank"
+            class="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-5 rounded-lg shadow-md transition-transform hover:scale-105">
+            💬 Chat WhatsApp
+        </a>
+
+        <a href="https://www.instagram.com/vvahyu_173" target="_blank"
+            class="bg-pink-500 hover:bg-pink-600 text-white font-bold py-2 px-5 rounded-lg shadow-md transition-transform hover:scale-105">
+            📷 Instagram
+        </a>
+
+        <a href="https://www.youtube.com/@usernamekamu" target="_blank"
+            class="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-5 rounded-lg shadow-md transition-transform hover:scale-105">
+            ▶️ YouTube
+        </a>
+</x-layout>
---
+++ home.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    
+    <script src="js/script.js"></script>
+</x-layout>
+
